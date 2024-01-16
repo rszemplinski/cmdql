e30fc97490e70f9647f523f59a170db599a5b8d7
Ryan Szemplinski <ryan.szemplinski@pieinsurance.com>
2024-01-15 21:14:48 -0700

    Update docfx version and platforms in default.nix

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
-{ pkgs ? import <nixpkgs> {}, lib ? pkgs.lib, buildDotnetGlobalTool ? pkgs.buildDotnetGlobalTool }:
+{ pkgs ? import <nixpkgs> { }, lib ? pkgs.lib, buildDotnetGlobalTool ? pkgs.buildDotnetGlobalTool }:
 
-let 
-    docfx = buildDotnetGlobalTool {
-        pname = "docfx";
-        version = "2.75.1";
-        nugetSha256 = "sha256-bbD4+yNxM4vmZXPczeFH+Hy5IohKVA2cIrb+88tLD8Y=";
-        meta = with lib; {
-            description = "Documentation Generation Tool";
-            homepage = "https://dotnet.github.io/docfx/";
-            license = licenses.mit;
-            platforms = platforms.all;
-        };
+let
+  docfx = buildDotnetGlobalTool {
+    pname = "docfx";
+    version = "2.75.1";
+    nugetSha256 = "sha256-bbD4+yNxM4vmZXPczeFH+Hy5IohKVA2cIrb+88tLD8Y=";
+    meta = with lib; {
+      description = "Documentation Generation Tool";
+      homepage = "https://dotnet.github.io/docfx/";
+      license = licenses.mit;
+      platforms = [ builtins.currentSystem ];
+    };
   };
 in
 pkgs.mkShell {
